0f3505f99606ba71df1178c87ef5a796c57f2571
leey00nsu <dbstndla1212@naver.com>
2022-06-03 20:43:18 +0900

    7주차

--- w7/src/App.jsx
+++ w7/src/App.jsx
@@ -37,42 +37,81 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { GlobalStyles, darkTheme, lightTheme } from "./styles";
+import { ThemeProvider } from "styled-components";
+import loadingIcon from "./loading.svg";
 
 function App() {
+  const darkMode = false;
+  const loading = false;
+  const isPost = true;
+
   return (
     <>
-      <MediaDiv>
-        <Header>
-          <TitleLogoDiv>
-            <TitleBig>멋사</TitleBig>
-            <TitleSmall>익명게시판</TitleSmall>
-          </TitleLogoDiv>
-          <SubHeaderDiv>다크모드버튼</SubHeaderDiv>
-        </Header>
-        <Main>
-          <SlogunSection>
-            <SlogunBig>Hack Your LIFE</SlogunBig>
-            <SlogunSmall>내 아이디어를 내 손으로 실현한다.</SlogunSmall>
-          </SlogunSection>
-          <PostSection>
-            <PostTitleDiv>
-              <FontAwesomeIcon icon={faArrowsRotate} />
-              <PostTitle>익명게시판</PostTitle>
-              <FontAwesomeIcon icon={faPenToSquare} />
-            </PostTitleDiv>
-            <PostListDiv>
-              <ul>
-                <EachPostLi>
-                  <div>
-                    <FontAwesomeIcon icon={faLocationPin} />
-                    <PostLink>제목제목제목</PostLink>
-                  </div>
-                </EachPostLi>
-              </ul>
-            </PostListDiv>
-          </PostSection>
-        </Main>
-      </MediaDiv>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <MediaDiv>
+          <Header>
+            <TitleLogoDiv>
+              <TitleBig>멋사</TitleBig>
+              <TitleSmall>익명게시판</TitleSmall>
+            </TitleLogoDiv>
+            <SubHeaderDiv>
+              {darkMode ? (
+                <FontAwesomeIcon icon={faMoon} />
+              ) : (
+                <FontAwesomeIcon icon={faSun} />
+              )}
+            </SubHeaderDiv>
+          </Header>
+          <Main>
+            <SlogunSection>
+              <SlogunBig>Hack Your LIFE</SlogunBig>
+              <SlogunSmall>내 아이디어를 내 손으로 실현한다.</SlogunSmall>
+            </SlogunSection>
+            <PostSection>
+              <PostTitleDiv>
+                <FontAwesomeIcon icon={faArrowsRotate} />
+                <PostTitle>익명게시판</PostTitle>
+                <FontAwesomeIcon icon={faPenToSquare} />
+              </PostTitleDiv>
+              <PostListDiv>
+                {loading ? (
+                  <LoadingDiv>
+                    <LoadingImg src={loadingIcon} />
+                  </LoadingDiv>
+                ) : isPost ? (
+                  <LoadingDiv>아직 기록된 글이 없습니다.</LoadingDiv>
+                ) : (
+                  <ul>
+                    <EachPostLi>
+                      <div>
+                        <FontAwesomeIcon icon={faLocationPin} />
+                        <PostLink>제목제목제목</PostLink>
+                      </div>
+                      <PostRepl>[10]</PostRepl>
+                    </EachPostLi>
+                  </ul>
+                )}
+              </PostListDiv>
+            </PostSection>
+            <PagingSection>
+              <PagenumberDiv>
+                <FontAwesomeIcon icon={faArrowLeft} />
+              </PagenumberDiv>
+              <PagenumberDiv>2</PagenumberDiv>
+              <PagenumberDiv>
+                <FontAwesomeIcon icon={faArrowRight} />
+              </PagenumberDiv>
+            </PagingSection>
+          </Main>
+          <Footer>
+            <FontAwesomeIcon icon={faReact} />
+            <FooterBig>for react Study</FooterBig>
+            <FooterSmall>2022 by Yoonsu</FooterSmall>
+          </Footer>
+        </MediaDiv>
+      </ThemeProvider>
     </>
   );
 }
