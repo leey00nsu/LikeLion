a36d364ccaaa1493b8c6dcafd6d25e550ccd720f
leeyoonsu <dbstndla1212@naver.com>
2022-09-16 19:54:44 +0900

    axios async

---
+++ w2-1/newsapp/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      axios
+        .get("https://newsapi.org/v2/top-headlines?country=kr&apiKey={APIKEY}")
+        .then((response) => {
+          setData(response.data);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
