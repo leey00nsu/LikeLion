99b72fcf2d0becef866932086ecdb2ad21ed6ffa
leeyoonsu <dbstndla1212@naver.com>
2022-09-30 18:58:56 +0900

    add w9

---
+++ w9/src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import axios from 'axios';
+import NewsList from './components/NewsList';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=2091564701e1446690302f66d6d0d8dc',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <NewsList/>
+      )}
+    </div>
+  );
+};
+
+export default App;
